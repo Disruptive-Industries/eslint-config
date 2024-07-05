1a562cc3882e3392b2e6f49b07363de592797f26
Antony Derham <antony.d@disruptive-industries.io>
2024-07-05 13:56:08 +0100

    fix: Add import linting rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,18 @@
-import compat from "eslint-plugin-compat";
-import globals from "globals";
+import { fixupPluginRules } from "@eslint/compat";
 import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import legacyNextPlugin from "@next/eslint-plugin-next";
 import eslintConfigPrettier from "eslint-config-prettier";
-import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import compat from "eslint-plugin-compat";
+import legacyImportPlugin from "eslint-plugin-import";
 import reactJSXRuntime from "eslint-plugin-react/configs/jsx-runtime.js";
-import nextPlugin from "@next/eslint-plugin-next";
-import hooksPlugin from "eslint-plugin-react-hooks";
+import reactRecommended from "eslint-plugin-react/configs/recommended.js";
+import legacyHooksPlugin from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+const nextPlugin = fixupPluginRules(legacyNextPlugin);
+const hooksPlugin = fixupPluginRules(legacyHooksPlugin);
+const importPlugin = fixupPluginRules(legacyImportPlugin);
 
 export default tseslint.config(
   eslint.configs.recommended,
@@ -19,14 +25,17 @@ export default tseslint.config(
     plugins: {
       "react-hooks": hooksPlugin,
       "@next/next": nextPlugin,
+      import: importPlugin,
     },
 
     languageOptions: {
+      parser: tseslint.parser,
       parserOptions: {
         project: "./tsconfig.json",
       },
       globals: {
         ...globals.browser,
+        ...globals.serviceworker,
       },
     },
 
@@ -34,12 +43,20 @@ export default tseslint.config(
       react: {
         version: "detect",
       },
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+        },
+        node: true,
+      },
     },
 
     rules: {
       // Legacy shared config rules
       ...hooksPlugin.configs.recommended.rules,
       ...nextPlugin.configs["core-web-vitals"].rules,
+      ...importPlugin.configs["recommended"].rules,
+      ...importPlugin.configs["typescript"].rules,
 
       // DI-specific rules
       "@typescript-eslint/no-misused-promises": [
@@ -52,6 +69,16 @@ export default tseslint.config(
       ],
       "@typescript-eslint/consistent-type-imports": "error",
       "object-shorthand": "error",
+      "import/no-amd": "error",
+      "import/no-commonjs": "error",
+      "import/no-absolute-path": "error",
+      "import/no-dynamic-require": "error",
+      "import/no-self-import": "error",
+      "import/no-useless-path-segments": "error",
+      "import/order": [
+        "error",
+        { "newlines-between": "always", alphabetize: { order: "asc" } },
+      ],
     },
   }
 );
